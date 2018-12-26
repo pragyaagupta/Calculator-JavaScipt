9cce15056767da41093300d46e72d98c6b2f10ea
Pragya <42120039+pragyaagupta@users.noreply.github.com>
2018-12-26 15:07:23 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,27 @@
+function insert(num){
+
+	document.form.textview.value=document.form.textview.value+num;
+}
+
+function equal(){
+
+	var exp=document.form.textview.value;
+	if(exp){
+
+      document.form.textview.value=eval(exp);
+	}
+}
+
+function clean(){
+	var exp=document.form.textview.value;
+	if(exp){
+
+	document.form.textview.value="";
+}
+}
+
+function back(){
+
+	var exp=document.form.textview.value;
+	document.form.textview.value=exp.substring(0,exp.length-1);
+}
